994a548dd0dded5763ea91c777a8897deb69a0f6
Rawnly <fedevitale99@gmail.com>
2017-06-16 00:39:54 +0200

    RENDER PROCESS

---
+++ app/js/index.js
@@ -0,0 +1,11 @@
+const {ipcRenderer} = require('electron');
+
+const ipc = ipcRenderer;
+
+$('#download').on('click', () => {
+	ipc.send('download', $('#idOrUrl')[0].value)
+});
+
+$('#random').on('click', () => {
+	ipc.send('download', 'random')
+});
